3325aa535bcff397ea9231a3caa8871283baa9ba
Dennis Effing <dennis.effing@codecentric.de>
2021-12-15 21:58:14 +0100

    Extract quality updates into when with expressions

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,50 +5,47 @@ class GildedRose(var items: Array<Item>) {
     fun updateQuality() {
         for (i in items) {
             i.apply {
-                when(name) {
-                    ItemType.BRIE.itemName -> {
-                        if (sellIn > 0) {
-                            quality++
-                        } else {
-                            quality += 2
-                        }
-                        if (quality > 50) {
-                            quality = 50
-                        }
-                    }
-                    ItemType.BACKSTAGE_PASSES.itemName -> {
-                        if (sellIn > 10) {
-                            quality++
-                        } else if (sellIn > 5) {
-                            quality += 2
-                        } else if (sellIn > 0) {
-                            quality += 3
-                        } else {
-                            quality = 0
-                        }
-                        if (quality > 50) {
-                            quality = 50
-                        }
-                    }
-                    ItemType.SULFURAS.itemName -> {
-                        // do nothing
-                    }
-                    else -> {
-                        if (sellIn > 0) {
-                            quality--
-                        } else {
-                            quality -= 2
-                        }
-                        if (quality < 0) {
-                            quality = 0
-                        }
-                    }
+                quality = when (name) {
+                    ItemType.BRIE.itemName -> limitQuality(
+                            if (sellIn > 0) {
+                                quality + 1
+                            } else {
+                                quality + 2
+                            })
+                    ItemType.BACKSTAGE_PASSES.itemName -> limitQuality(
+                            if (sellIn > 10) {
+                                quality + 1
+                            } else if (sellIn > 5) {
+                                quality + 2
+                            } else if (sellIn > 0) {
+                                quality + 3
+                            } else {
+                                0
+                            })
+                    ItemType.SULFURAS.itemName -> quality
+                    else -> limitQuality(
+                            if (sellIn > 0) {
+                                quality - 1
+                            } else {
+                                quality - 2
+                            })
                 }
-                sellIn = when(name) {
+
+                sellIn = when (name) {
                     ItemType.SULFURAS.itemName -> sellIn
                     else -> sellIn - 1
                 }
             }
+        }
+    }
+
+    fun limitQuality(quality: Int): Int {
+        return if (quality > 50) {
+            50
+        } else if (quality < 0) {
+            0
+        } else {
+            quality
         }
     }
 
