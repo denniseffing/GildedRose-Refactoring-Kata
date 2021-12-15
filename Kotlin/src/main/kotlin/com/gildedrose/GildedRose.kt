ed5e0fef5675dd756857227f0c149ad62c69d10c
Danny Textores <danny.textores@inovex.de>
2021-12-15 21:46:10 +0100

    Simplify nested ifs based on item names

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,51 +5,46 @@ class GildedRose(var items: Array<Item>) {
     fun updateQuality() {
         for (i in items) {
             i.apply {
-                if (name != ItemType.BRIE.itemName && name != ItemType.BACKSTAGE_PASSES.itemName) {
-                    if (quality > 0) {
-                        if (name != ItemType.SULFURAS.itemName) {
-                            quality -= 1
+                when(name) {
+                    ItemType.BRIE.itemName -> {
+                        if (sellIn > 0) {
+                            quality++
+                        } else {
+                            quality += 2
                         }
-                    }
-                } else {
-                    if (quality < 50) {
-                        quality += 1
-
-                        if (name == ItemType.BACKSTAGE_PASSES.itemName) {
-                            if (sellIn < 11) {
-                                if (quality < 50) {
-                                    quality += 1
-                                }
-                            }
-
-                            if (sellIn < 6) {
-                                if (quality < 50) {
-                                    quality += 1
-                                }
-                            }
+                        if (quality > 50) {
+                            quality = 50
                         }
+                        sellIn--
                     }
-                }
-
-                if (name != ItemType.SULFURAS.itemName) {
-                    sellIn -= 1
-                }
-
-                if (sellIn < 0) {
-                    if (name != ItemType.BRIE.itemName) {
-                        if (name != ItemType.BACKSTAGE_PASSES.itemName) {
-                            if (quality > 0) {
-                                if (name != ItemType.SULFURAS.itemName) {
-                                    quality -= 1
-                                }
-                            }
+                    ItemType.BACKSTAGE_PASSES.itemName -> {
+                        if (sellIn > 10) {
+                            quality++
+                        } else if (sellIn > 5) {
+                            quality += 2
+                        } else if (sellIn > 0) {
+                            quality += 3
                         } else {
                             quality = 0
                         }
-                    } else {
-                        if (quality < 50) {
-                            quality += 1
+                        if (quality > 50) {
+                            quality = 50
                         }
+                        sellIn--
+                    }
+                    ItemType.SULFURAS.itemName -> {
+                        // do nothing
+                    }
+                    else -> {
+                        if (sellIn > 0) {
+                            quality--
+                        } else {
+                            quality -= 2
+                        }
+                        if (quality < 0) {
+                            quality = 0
+                        }
+                        sellIn--
                     }
                 }
             }
